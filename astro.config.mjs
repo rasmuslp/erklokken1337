44d07ee0286a1f1e634645f5c6508bc272cd29c6
Rasmus Ljungmann Pedersen <rasmuslp@gmail.com>
2023-06-20 22:02:41 +0200

    feat: Add TailwindCSS and style with that

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 import robotsTxt from 'astro-robots-txt';
-
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://erklokken1337.dk',
-  integrations: [robotsTxt({
-    policy: [{
-      userAgent: '*',
-      disallow: '\,'
-    }]
-  }), sitemap({
-    lastmod: new Date()
-  })]
+  integrations: [
+      robotsTxt({
+        policy: [{
+          userAgent: '*',
+          disallow: '\,'
+        }]
+      }),
+      sitemap({
+        lastmod: new Date()
+      }),
+      tailwind()
+    ]
 });
